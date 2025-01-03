074c22e1ac07f462cd6d15606170f6f4168ef08a
Shehin Thoombil <shehinthoombil@gmail.com>
2025-01-03 18:25:25 +0530

    vercel error testing2

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,36 +1,9 @@
-import { Navigate, useNavigate, Outlet, useLocation } from 'react-router-dom';
+import { Navigate, useNavigate, Outlet } from 'react-router-dom';
 import React, { useState } from 'react';
 import { SidebarItem } from "./SidebarItem";
 import { UsersRound, LayoutDashboard, Menu, X ,Gift ,User } from 'lucide-react';
 import { LogOut } from 'lucide-react';
-const  SidebarItem= ({ icon, label, navigate_to })=> {
-    const navigate = useNavigate();
-    const location = useLocation();
-    
-    // Determine if the current route matches the `navigate_to` route
-    const isActive = location.pathname === navigate_to;
-  
-    return (
-      <div onClick={() => navigate(navigate_to)}
-        className={`flex  flex-col justify-center cursor-pointer py-4 px-28 pl-4 w-full ${
-          isActive
-            ? "font-bold text-amber-500 bg-orange-50 border-solid border-r-[3px] border-r-amber-500"
-            : "bg-white"
-        } max-w-[250px]`}
-      >
-        <div className="flex pl-2 gap-2.5 items-center w-full">
-          <div className="font-semibold">{icon}</div>
-          <div
-            
-            className="flex-1 shrink self-stretch my-auto basis-0 cursor-pointer"
-          >
-            {label}
-          </div>
-        </div>
-      </div>
-    );
-  }
-  
+
 const ProtectedRoute=()=> {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
