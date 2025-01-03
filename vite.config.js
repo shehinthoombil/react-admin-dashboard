d0f7d7a3eb508703bc59f7013dd43a1428d7e833
Shehin Thoombil <shehinthoombil@gmail.com>
2025-01-03 18:01:30 +0530

    changed to lazy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';  
 
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,5 +12,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
+  
 });
